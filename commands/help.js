81929aa7bd3317bd9d9aba604c97e6eacb0a65f0
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2022-01-13 13:31:46 +0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,22 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
-  const commands = `connect\`\` - join the voice channel you are in
-   disconnect\`\` - leave the voice channel you are in
-   play <Song Name or url>\`\` - play songs from youtube
+  const commands = `connect\`\` - joins the voice channel you are in
+   disconnect\`\` - leaves the voice channel you are in
+   play [song name or youtube url]\`\` - plays songs from youtube
    pause\`\` - pause currently playing songs in the server
    resume\`\` - resume paused songs in the server
    queue\`\` - shows the song queue of the server
-   skip\`\` - skips to next song in the queue
-   skipto <Target number>\`\` - Multiple skips until target
+   skip\`\` - skips to the next song in the queue
+   skipto [valid integer]\`\` - skips multiple songs from the queue
    stop\`\` - stops the song and clears the queue
-   volume <volume count or none>\`\` - see or adjust volume of songs
-   np\`\` - see now playing song
-   lyrics\`\` - get lyrics of current song
+   volume [valid integer 1-100]\`\` - see or adjust volume of songs
+   np\`\` - see the now playing songs info
+   lyrics\`\` - get lyrics for currently playing song
    shuffle\`\` - shuffle and randomize the queue
-   loop\`\` - enable / disable loop for the currently playing song
-   remove <Target number>\`\` - remove a song from the queue
-   help\`\` - to see this command`;
+   loop\`\` - enable/disable loop for the currently playing song
+   remove [valid integer]\`\` - remove a song from the queue
+   help\`\` - shows you this message about all commands and their usages`;
 
   const revised = commands
     .split("\n")
