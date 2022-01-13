6a194621302dba3e057d861f11dd9c66486d1fc5
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2022-01-13 13:43:46 +0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,18 +4,18 @@ exports.run = async (client, message) => {
   const commands = `connect\`\` - joins the voice channel you are in
    disconnect\`\` - leaves the voice channel you are in
    play [song name or youtube url]\`\` - plays songs from youtube
-   pause\`\` - pause currently playing songs in the server
-   resume\`\` - resume paused songs in the server
+   pause\`\` - pauses currently playing songs in the server
+   resume\`\` - resumes paused songs in the server
    queue\`\` - shows the song queue of the server
    skip\`\` - skips to the next song in the queue
    skipto [valid integer]\`\` - skips multiple songs from the queue
    stop\`\` - stops the song and clears the queue
-   volume [valid integer 1-100]\`\` - see or adjust volume of songs
-   np\`\` - see the now playing songs info
-   lyrics\`\` - get lyrics for currently playing song
-   shuffle\`\` - shuffle and randomize the queue
+   volume [valid integer 1-100]\`\` - adjusts volume & shows volume
+   songinfo\`\` - shows the currently playing song info
+   lyrics\`\` - gets lyrics for currently playing song
+   shuffle\`\` - shuffles and randomizes the queue
    loop\`\` - enable/disable loop for the currently playing song
-   remove [valid integer]\`\` - remove a song from the queue
+   remove [valid integer]\`\` - removes a song from the queue
    help\`\` - shows you this message about all commands and their usages`;
 
   const revised = commands
